5714103f9e023764bc6b94a4840583185f4ab4cb
Onur Yilmazer <onur.yilmazer@uni-jena.de>
2024-03-12 21:44:18 +0100

    Added new plots for the benchmarks

--- project/benchmarks/threadsbenchmark_plot.R
+++ project/benchmarks/threadsbenchmark_plot.R
@@ -1,8 +1,10 @@
 library(ggplot2)
 library(scales)
 
-debugdata <- read.csv("threads_benchmark_debug.csv")
-releasedata <- read.csv("threads_benchmark_release.csv")
+
+#Benchmark 1
+debugdata <- read.csv("threads_benchmark_grayscaleconversion(debug).csv")
+releasedata <- read.csv("threads_benchmark_grayscaleconversion(release).csv")
 
 debugdata$type <- "debug"
 releasedata$type <- "release"
@@ -15,9 +17,59 @@ ggplot(data = combineddata, mapping = aes(x = number_of_threads, y = runtime_in_
   geom_smooth() +
   theme_minimal() +
   scale_x_continuous(breaks = 1:max(combineddata$number_of_threads)) +
-  labs(title = "Benchmark performance of 'Enhancer' based on the nr of threads",
+  labs(title = "Runtime of parallelized grayscale conversion",
        x = "Number of Threads",
        y = "Runtime in Seconds",
-       color = "Compilation mode")
+       color = "Compilation mode") +
+  expand_limits(y = 0)  # Ensure y-axis starts from 0
 
-ggsave("threads_benchmark_combined.png", device = "png", width = 1920, height = 1080, units = "px", dpi = 150, bg="white")
+ggsave("parallelgrayscale_benchmark.png", device = "png", width = 1920, height = 1080, units = "px", dpi = 150, bg="white")
+
+
+
+#Benchmark 2
+debugdata <- read.csv("threads_benchmark_adaptivethresholding(debug).csv")
+releasedata <- read.csv("threads_benchmark_adaptivethresholding(release).csv")
+
+debugdata$type <- "debug"
+releasedata$type <- "release"
+
+combineddata <- rbind(debugdata, releasedata)
+
+ggplot(data = combineddata, mapping = aes(x = number_of_threads, y = runtime_in_seconds, color = type)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth() +
+  theme_minimal() +
+  scale_x_continuous(breaks = 1:max(combineddata$number_of_threads)) +
+  labs(title = "Runtime of parallelized adaptive thresholding + sequential grayscale conversion",
+       x = "Number of Threads",
+       y = "Runtime in Seconds",
+       color = "Compilation mode") +
+  expand_limits(y = 0)  # Ensure y-axis starts from 0
+
+ggsave("paralleladaptive_sequentialgrayscale_benchmark.png", device = "png", width = 1920, height = 1080, units = "px", dpi = 150, bg="white")
+
+
+#Benchmark 3
+debugdata <- read.csv("threads_benchmark_adaptivethresholding(debug).csv")
+releasedata <- read.csv("threads_benchmark_adaptivethresholding(release).csv")
+
+debugdata$type <- "debug"
+releasedata$type <- "release"
+
+combineddata <- rbind(debugdata, releasedata)
+
+ggplot(data = combineddata, mapping = aes(x = number_of_threads, y = runtime_in_seconds, color = type)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth() +
+  theme_minimal() +
+  scale_x_continuous(breaks = 1:max(combineddata$number_of_threads)) +
+  labs(title = "Runtime of parallelized adaptive thresholding + parallelized grayscale conversion",
+       x = "Number of Threads",
+       y = "Runtime in Seconds",
+       color = "Compilation mode") +
+  expand_limits(y = 0)  # Ensure y-axis starts from 0
+
+ggsave("paralleladaptive_parallelgrayscale_benchmark.png", device = "png", width = 1920, height = 1080, units = "px", dpi = 150, bg="white")
