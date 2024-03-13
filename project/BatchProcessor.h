81be944492f8b3ec292131167ad7ce7142461b58
Onur Yılmazer <onur.yilmazer@uni-jena.de>
2024-03-13 22:39:34 +0100

    Added a description comment to BatchProcessor.h

--- project/BatchProcessor.h
+++ project/BatchProcessor.h
@@ -3,6 +3,12 @@
 
 #include "CommandLineInterface.h"
 
+/*
+    This class takes a CommandLineInterface instance in its constructor, and uses the user inputs
+    that are stored in this CommandLineInterface instance; it loads the pictures from the inputPath
+    given by the user and processes them, then saves them inside the outputDirectory provided by the user, 
+    one by one.
+*/
 
 class BatchProcessor {
 public:
